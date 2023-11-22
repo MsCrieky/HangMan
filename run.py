8fc73a997a825d5a0a717c4bb8a7c4bc38d49fcc
MsCrieky <>
2023-11-22 10:33:25 +0000

    Add ascii art as welcome

--- run.py
+++ run.py
@@ -3,4 +3,35 @@
 import dictionary
 import hangman
 import random
+from rich import print
+
+print("Welcome to: \n")
+print()
+print("[bold green] __    __       ___      .__   __.   _______ .___  ___.      ___      .__   __.[/bold green]")
+print("[bold green]|  |  |  |     /   \     |  \ |  |  /  _____||   \/   |     /   \     |  \ |  |[/bold green]")
+print("[bold green]|  |__|  |    /  ^  \    |   \|  | |  |  __  |  \  /  |    /  ^  \    |   \|  |[/bold green]") 
+print("[bold green]|   __   |   /  /_\  \   |  . `  | |  | |_ | |  |\/|  |   /  /_\  \   |  . `  |[/bold green]") 
+print("[bold green]|  |  |  |  /  _____  \  |  |\   | |  |__| | |  |  |  |  /  _____  \  |  |\   |[/bold green]") 
+print("[bold green]|__|  |__| /__/     \__\ |__| \__|  \______| |__|  |__| /__/     \__\ |__| \__|[/bold green]")
+print("[bold green]===============================================================================[/bold green]\n")
+
+print(" ___________.._______ ")
+print("| .__________))______| ")
+print(" | | / /      || ")
+print(" | |/ /       || ")
+print(" | | /        ||.-''. ")
+print(" | |/         |/  _  \ ")
+print(" | |          ||  `/,| ")
+print(" | |          (\\`_.' ")
+print(" | |         .-`--'. ")
+print(" | |        /Y . .Y\ ")
+print(" | |       // |   |\\ ")
+print(" | |      //  | . | \\ ")
+print(" | |     ')   |   |  (` ")
+print(" | |          ||'|| ")
+print(" | |          || || ")
+print(" | |          || || ")
+print(" | |          || || ")
+print(" | |         /     \ ")
+print(" ====================== \n")
 
